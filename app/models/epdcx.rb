MITLibraries/QuickSubmit

commit c96f8fc87feff4e469b8f36177cb5ed2ccee0eae

Include Callback URI in mets.xml

app/models/mets.rb
@@ -1,15 +1,8 @@
 class Mets
-  def initialize(submission)
+  def initialize(submission, callback_uri)
     @submission = submission
-    @builder = Nokogiri::XML::Builder.new do |xml|
-      xml['mets'].mets(
-        'xmlns:mets' => 'http://www.loc.gov/METS/',
-        'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
-        'xmlns:epdcx' => 'http://purl.org/eprint/epdcx/2006-11-16/',
-        'PROFILE' => 'DSpace METS SIP Profile 1.0') do
-        overall_structure(xml)
-      end
-    end
+    @callback_uri = callback_uri
+    @builder = xml_builder
   end
 
   def to_xml
@@ -18,9 +11,23 @@ class Mets
 
   private
 
+  def xml_builder
+    Nokogiri::XML::Builder.new do |xml|
+      xml['mets'].mets(
+        'xmlns:mets' => 'http://www.loc.gov/METS/',
+        'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
+        'xmlns:epdcx' => 'http://purl.org/eprint/epdcx/2006-11-16/',
+        'xmlns:callback' => '',
+        'PROFILE' => 'DSpace METS SIP Profile 1.0') do
+        overall_structure(xml)
+      end
+    end
+  end
+
   def overall_structure(xml)
     header(xml)
     dmd(xml)
+    amd(xml)
     filesec(xml)
     structmap(xml)
   end
@@ -30,6 +37,18 @@ class Mets
       xml['mets'].agent('ROLE' => 'DISSEMINATOR',
                         'TYPE' => 'ORGANIZATION') do
         xml['mets'].name('MIT Libraries: QuickSubmit')
+      end
+    end
+  end
+
+  def amd(xml)
+    xml['mets'].amdSec('ID' => 'mitqs_amd') do
+      xml['mets'].techMD('ID' => 'mitqs_amd_tech') do
+        xml['mets'].mdWrap('MDTYPE' => 'OTHER', 'MIMETYPE' => 'text/xml') do
+          xml['mets'].xmlData do
+            xml['callback'].callback_uri(@callback_uri)
+          end
+        end
       end
     end
   end

app/controllers/submissions_controller.rb
@@ -32,8 +32,12 @@ class SubmissionsController < ApplicationController
 
   private
 
+  def callback_uri
+    callback_submission_status_url(@submission)
+  end
+
   def process_submission
-    @submission.to_sword_package
+    @submission.to_sword_package(callback_uri)
     sword = Sword.new(@submission)
     begin
       sword.deposit

app/models/submission.rb
@@ -35,8 +35,8 @@ class Submission < ActiveRecord::Base
     status == 'approved'
   end
 
-  def to_mets
-    Mets.new(self).to_xml
+  def to_mets(callback)
+    Mets.new(self, callback).to_xml
   end
 
   def sword_path
@@ -48,12 +48,12 @@ class Submission < ActiveRecord::Base
     self.uuid = SecureRandom.uuid
   end
 
-  def to_sword_package
+  def to_sword_package(callback)
     Zip::File.open(sword_path, Zip::File::CREATE) do |zipfile|
       documents.each do |document|
         zipfile.add(document.file.filename, document.file.file)
       end
-      zipfile.get_output_stream('mets.xml') { |os| os.write to_mets }
+      zipfile.get_output_stream('mets.xml') { |os| os.write to_mets(callback) }
     end
   end
 end

test/features/submission_generated_sword_tests.rb
@@ -1,0 +1,35 @@
+require 'test_helper'
+
+class SubmissionGeneratedSwordTest < Capybara::Rails::TestCase
+  def setup
+    auth_setup
+    FileUtils.rm_f('tmp/69b9156a124c96bbdb55cad753810e14.zip')
+    FileUtils.rm_f('tmp/40550618d6b4d97792b0773c97207186.zip')
+  end
+
+  def teardown
+    auth_teardown
+    @sub.documents.map(&:remove!) if @sub
+  end
+
+  def base_valid_form
+    mock_auth(users(:one))
+    visit new_submission_path
+    fill_in('Journal', with: 'Super Mega Journal')
+    fill_in('Title', with: 'Alphabetical Order is Good Enough')
+    check('I am authorized to submit this article.')
+  end
+
+  test 'mets xml contains expected callback_uri' do
+    base_valid_form
+    attach_file('submission[documents][]',
+                File.absolute_path('./test/fixtures/a_pdf.pdf'))
+    VCR.use_cassette('a submission', preserve_exact_body_bytes: true) do
+      click_on('Create Submission')
+    end
+    @sub = Submission.last
+    mets = Zip::File.open(@sub.sword_path).glob('mets.xml').first
+           .get_input_stream.read
+    assert(mets.include?('http://www.example.com/callbacks/status/783478148'))
+  end
+end

test/fixtures/submissions.yml
@@ -26,6 +26,7 @@ sub_one:
   agreed_to_license: true
   documents: "[\"b_pdf.pdf\", \"a_pdf.pdf\"]"
   user: one
+  uuid: 7648b505-af79-4a2b-abcb-2727f716100c
 
 sub_two:
   title: 'Simple Secret Substitution Songs'
@@ -37,3 +38,4 @@ sub_two:
   grant_number: 'asdf123'
   agreed_to_license: true
   documents: "[\"b_pdf.pdf\"]"
+  uuid: a481170a-8337-4adc-b5b6-289b1037bd76

test/models/mets_test.rb
@@ -5,7 +5,7 @@ class MetsTest < ActiveSupport::TestCase
     Dir.chdir("#{Rails.root}/test/fixtures/schemas") do
       sub = submissions(:sub_two)
       xsd = Nokogiri::XML::Schema(File.read('mets.xsd'))
-      xml = Mets.new(sub).to_xml
+      xml = Mets.new(sub, 'http://example.com/callback').to_xml
       doc = Nokogiri::XML(xml)
       assert_equal(true, xsd.valid?(doc))
     end
@@ -15,7 +15,7 @@ class MetsTest < ActiveSupport::TestCase
     Dir.chdir("#{Rails.root}/test/fixtures/schemas") do
       sub = submissions(:sub_one)
       xsd = Nokogiri::XML::Schema(File.read('mets.xsd'))
-      xml = Mets.new(sub).to_xml
+      xml = Mets.new(sub, 'http://example.com/callback').to_xml
       doc = Nokogiri::XML(xml)
       assert_equal(true, xsd.valid?(doc))
     end

test/models/submission_test.rb
@@ -81,7 +81,7 @@ class SubmissionTest < ActiveSupport::TestCase
     Dir.chdir("#{Rails.root}/test/fixtures/schemas") do
       sub = submissions(:sub_two)
       xsd = Nokogiri::XML::Schema(File.read('mets.xsd'))
-      doc = Nokogiri::XML(sub.to_mets)
+      doc = Nokogiri::XML(sub.to_mets('http://example.com/callback'))
       assert_equal(true, xsd.valid?(doc))
     end
   end
@@ -106,7 +106,7 @@ class SubmissionTest < ActiveSupport::TestCase
   test '#to_sword_package creates zip file' do
     sub = submissions(:sub_one)
     setup_sword_files(sub)
-    sub.to_sword_package
+    sub.to_sword_package('http://example.com/callback')
     assert_equal(true, File.file?(sub.sword_path))
     cleaup_sword_files(sub)
   end
@@ -114,7 +114,7 @@ class SubmissionTest < ActiveSupport::TestCase
   test '#to_sword_package zip contains correct files' do
     sub = submissions(:sub_one)
     setup_sword_files(sub)
-    sub.to_sword_package
+    sub.to_sword_package('http://example.com/callback')
     sword = Zip::File.open(sub.sword_path)
     assert_equal(['a_pdf.pdf', 'b_pdf.pdf', 'mets.xml'], sword.map(&:name).sort)
     cleaup_sword_files(sub)

test/models/sword_test.rb
@@ -17,7 +17,7 @@ class SwordTest < ActiveSupport::TestCase
     VCR.use_cassette('workflow submission', preserve_exact_body_bytes: true) do
       sub = submissions(:sub_one)
       setup_sword_files(sub)
-      sub.to_sword_package
+      sub.to_sword_package('http://example.com/callback')
       sword = Sword.new(sub)
       response = sword.deposit
       cleaup_sword_files(sub)
@@ -29,7 +29,7 @@ class SwordTest < ActiveSupport::TestCase
     VCR.use_cassette('deposit', preserve_exact_body_bytes: true) do
       sub = submissions(:sub_one)
       setup_sword_files(sub)
-      sub.to_sword_package
+      sub.to_sword_package('http://example.com/callback')
       sword = Sword.new(sub)
       response = sword.deposit
       cleaup_sword_files(sub)
@@ -41,7 +41,7 @@ class SwordTest < ActiveSupport::TestCase
     VCR.use_cassette('invalid credentials', preserve_exact_body_bytes: true) do
       sub = submissions(:sub_one)
       setup_sword_files(sub)
-      sub.to_sword_package
+      sub.to_sword_package('http://example.com/callback')
       sword = Sword.new(sub)
       assert_raises RestClient::Unauthorized do
         sword.deposit

commit 59c94fb983b98f826d1919901c894d242e4f43e9

Fixes bug with mets.xml
c96f8fc introduced a bug in which
DSpace was unable to parse the xml using a namespace with no
definition.

Additionally, we were unable to extract the callback URI from the XML
unless it was moved into EPDCX. I have no idea why and will leave that
as a challenge for someone in the future to solve.

app/models/epdcx.rb
@@ -1,6 +1,7 @@
 class Epdcx
-  def initialize(xml, submission)
+  def initialize(xml, submission, callback_uri)
     @xml = xml
+    @callback_uri = callback_uri
     @xml['epdcx'].descriptionSet(
       'xmlns:epdcx' => 'http://purl.org/eprint/epdcx/2006-11-16/') do
       work(submission)
@@ -27,6 +28,7 @@ class Epdcx
 
   def work(submission)
     @xml['epdcx'].description('epdcx:resourceId' => 'sword-mets-epdcx-1') do
+      statement('http://libraries.mit.edu/xmlns/callback', @callback_uri)
       scholarly_work
       statement('http://purl.org/dc/elements/1.1/title', submission.title)
       statement('http://purl.org/dc/elements/1.1/creator', submission.author)

app/models/mets.rb
@@ -17,7 +17,6 @@ class Mets
         'xmlns:mets' => 'http://www.loc.gov/METS/',
         'xmlns:xlink' => 'http://www.w3.org/1999/xlink',
         'xmlns:epdcx' => 'http://purl.org/eprint/epdcx/2006-11-16/',
-        'xmlns:callback' => '',
         'PROFILE' => 'DSpace METS SIP Profile 1.0') do
         overall_structure(xml)
       end
@@ -27,7 +26,6 @@ class Mets
   def overall_structure(xml)
     header(xml)
     dmd(xml)
-    amd(xml)
     filesec(xml)
     structmap(xml)
   end
@@ -37,18 +35,6 @@ class Mets
       xml['mets'].agent('ROLE' => 'DISSEMINATOR',
                         'TYPE' => 'ORGANIZATION') do
         xml['mets'].name('MIT Libraries: QuickSubmit')
-      end
-    end
-  end
-
-  def amd(xml)
-    xml['mets'].amdSec('ID' => 'mitqs_amd') do
-      xml['mets'].techMD('ID' => 'mitqs_amd_tech') do
-        xml['mets'].mdWrap('MDTYPE' => 'OTHER', 'MIMETYPE' => 'text/xml') do
-          xml['mets'].xmlData do
-            xml['callback'].callback_uri(@callback_uri)
-          end
-        end
       end
     end
   end
@@ -94,7 +80,7 @@ class Mets
 
   def xml_data(xml)
     xml['mets'].xmlData do
-      Epdcx.new(xml, @submission)
+      Epdcx.new(xml, @submission, @callback_uri)
     end
   end
 end

test/models/epdcx_test.rb
@@ -6,7 +6,7 @@ class EpdcxTest < ActiveSupport::TestCase
       sub = submissions(:sub_two)
       builder = Nokogiri::XML::Builder.new { |xml| xml }
       xsd = Nokogiri::XML::Schema(File.read('epdcx.xsd'))
-      xml = Epdcx.new(builder, sub).to_xml
+      xml = Epdcx.new(builder, sub, 'http://example.com').to_xml
       doc = Nokogiri::XML(xml)
       assert_equal(true, xsd.valid?(doc))
     end
@@ -17,7 +17,7 @@ class EpdcxTest < ActiveSupport::TestCase
       sub = submissions(:sub_one)
       builder = Nokogiri::XML::Builder.new { |xml| xml }
       xsd = Nokogiri::XML::Schema(File.read('epdcx.xsd'))
-      xml = Epdcx.new(builder, sub).to_xml
+      xml = Epdcx.new(builder, sub, 'http://example.com').to_xml
       doc = Nokogiri::XML(xml)
       assert_equal(true, xsd.valid?(doc))
     end