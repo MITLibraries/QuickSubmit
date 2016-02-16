94155de64613a87b1141c8200443ba792441e0f3
Jeremy Prevost <jprevost@mit.edu>
2016-02-16 15:58:38 -0500

    Allow all emails to be disabled
    Until sending emails from Heroku is resolved, we need a mechanism to
    disable all emails. This might never be merged… we’ll see how long
    solving this takes.

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -1,20 +1,24 @@
 class SubmissionMailer < ApplicationMailer
   def queued(submission)
+    return if ENV['DISABLE_ALL_EMAIL']
     @submission = submission
     mail(to: @submission.user.email, subject: 'Submission Received')
   end
 
   def deposited(submission)
+    return if ENV['DISABLE_ALL_EMAIL']
     @submission = submission
     mail(to: @submission.user.email, subject: 'Submission Complete')
   end
 
   def rejected(submission)
+    return if ENV['DISABLE_ALL_EMAIL']
     @submission = submission
     mail(to: @submission.user.email, subject: 'Submission Problem')
   end
 
   def failed(submission, error)
+    return if ENV['DISABLE_ALL_EMAIL']
     @submission = submission
     @error = error
     mail(to: User.where(admin: true).map(&:email),
